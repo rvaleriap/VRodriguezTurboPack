71288b1bf7eacdd26a8e155b7d022c1e9f6fa3b7
rvaleriap <rvaleriap1402@gmail.com>
2023-04-10 12:28:17 -0600

    Add project files.

---
+++ SL/Controllers/UsuarioController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SL.Controllers
+{
+    public class UsuarioController : Controller
+    {
+       
+        public ActionResult GetAll()
+        {
+            return View();
+        }
+    }
+}
---
+++ SL/wwwroot/js/JavaScript.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    GetAll();
+});
+
+function GetAll() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://randomuser.me/api',
+        success: function (result) {
+            $('#tblUsuario tbody');
+            $.each(result.results, function (i, empleado) {
+                var filas =
+                    '<tr>'
+                    + "<td class='text-center'>" + empleado.name.first  + "</td>"
+                    + "<td class='text-center'>" + empleado.email + "</ td>"
+                    + "<td class='text-center'>" + empleado.location.city + "</ td>"
+                    + "<td class='text-center'>" + empleado.gender + "</ td>"
+                    + "<td class='text-center'>" + '<img  style="height: 60px; width: 60px;" src= "' + empleado.picture.medium +'" />' + '</td>'
+                   
+
+                    + "</tr>";
+                $("#tblUsuario tbody").append(filas);
+            });
+
+        }
+    });
+}
+
+function Limpiar() {
+   
+    $('#tblUsuario tbody').empty();
+}
